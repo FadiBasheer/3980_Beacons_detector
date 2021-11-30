0e8e84b19a8e6faf6003df1a0ac672c847c1a8b7
FadiBasheer <fadih1992@gmail.com>
2021-11-30 08:28:59 -0800

    first

--- src/client.c
+++ src/client.c
@@ -118,7 +118,7 @@ int main(void) {
                                 getstr(str);
                                 if (strcmp(str, "1") == 0) {
                                     strcpy(data,
-                                           "GET / HTTP/1.1\nHost: 127.0.0.1:8081\nUser-Agent: curl/7.68.0\nAccept: */*\r\n\r\n");
+                                           "GET / HTTP/1.0\nHost: 127.0.0.1:8081\nUser-Agent: curl/7.68.0\nAccept: */*\r\n\r\n");
                                     dc_write(&env, &err, socket_fd, data, strlen(data));
                                     mvprintw(LINES - 2, 0, "You Entered: %s", str);
                                 } else if (strcmp(str, "2") == 0) {
--- src/server.c
+++ src/server.c
@@ -10,6 +10,10 @@
 #include <unistd.h>
 #include <string.h>
 #include "db.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 static void error_reporter(const struct dc_error *err);
 
@@ -18,11 +22,9 @@ static void trace_reporter(const struct dc_posix_env *env, const char *file_name
 
 static void quit_handler(int sig_num);
 
-void receive_data(struct dc_posix_env *env, struct dc_error *err, int fd, size_t size);
-
+int receive_data(struct dc_posix_env *env, struct dc_error *err, int fd, size_t size);
 
 static volatile sig_atomic_t exit_flag;
-
 
 int main(void) {
     dc_error_reporter reporter;
@@ -39,7 +41,8 @@ int main(void) {
     dc_error_init(&err, reporter);
     dc_posix_env_init(&env, tracer);
 
-    host_name = "127.0.0.1";
+    host_name = "192.168.0.18";
+    //host_name = "localhost";
     dc_memset(&env, &hints, 0, sizeof(hints));
     hints.ai_family = PF_INET; // PF_INET6;
     hints.ai_socktype = SOCK_STREAM;
@@ -107,12 +110,94 @@ int main(void) {
 
                             while (!(exit_flag) && dc_error_has_no_error(&err)) {
                                 int client_socket_fd;
-
+                                char *response;
                                 client_socket_fd = dc_accept(&env, &err, server_socket_fd, NULL, NULL);
 
                                 if (dc_error_has_no_error(&err)) {
-                                    receive_data(&env, &err, client_socket_fd, 1024);
+
+                                    int receive_status = receive_data(&env, &err, client_socket_fd, BUFSIZ);
+
+                                    switch (receive_status) {
+                                        case 1: {
+                                            // Get response
+                                            char respose_GET[] = "HTTP/1.0 200 OK\r\n"
+                                                                 "Date: Monday, 24-Apr-95 12:04:12 GMT\r\n"
+                                                                 "Content-type: text/html\r\n"
+                                                                 "\r\n"
+
+                                                                 "<!Doctype html>\r\n"
+                                                                 "<html>\r\n"
+                                                                 "<head>\r\n"
+                                                                 "<title>GET response</title>\r\n"
+                                                                 "</head>\r\n"
+                                                                 "<body>\r\n"
+                                                                 "<div>\r\n"
+                                                                 "<h1>Examplehgjhgjhjgjhfgjhgjhgjhjh Domain</h1>\r\n"
+                                                                 "</div>\r\n"
+                                                                 "</body>"
+                                                                 "</html>\r\n\r\n";
+                                            response = malloc(strlen(respose_GET) + 1);
+                                            strcpy(response, respose_GET);
+                                        }
+                                            break;
+                                        case 2: {
+                                            // PUT response
+                                            char respose_PUT[] = "HTTP/1.0 200 OK\r\n"
+                                                                 "Date: Monday, 24-Apr-95 12:04:12 GMT\r\n"
+                                                                 "Content-type: text/html\r\n"
+                                                                 "\r\n"
+
+                                                                 "<!Doctype html>\r\n"
+                                                                 "<html>\r\n"
+                                                                 "<head>\r\n"
+                                                                 "<title>PUT</title>\r\n"
+                                                                 "</head>\r\n"
+                                                                 "<body>\r\n"
+                                                                 "<div>\r\n"
+                                                                 "<h1>Data has been added/h1>\r\n"
+                                                                 "</div>\r\n"
+                                                                 "</body>"
+                                                                 "</html>\r\n\r\n";
+                                            response = malloc(strlen(respose_PUT) + 1);
+                                            strcpy(response, respose_PUT);
+                                        }
+                                            break;
+                                        default: {
+                                            // 500 response
+                                            char response_500[] = "HTTP/1.0 200 OK\r\n"
+                                                                  "Date: Monday, 24-Apr-95 12:04:12 GMT\r\n"
+                                                                  "Content-type: text/html\r\n"
+                                                                  "\r\n"
+
+                                                                  "<!Doctype html>\r\n"
+                                                                  "<html>\r\n"
+                                                                  "<head>\r\n"
+                                                                  "<title>500</title>\r\n"
+                                                                  "</head>\r\n"
+                                                                  "<body>\r\n"
+                                                                  "<div>\r\n"
+                                                                  "<h1>500</h1>\r\n"
+                                                                  "</div>\r\n"
+                                                                  "</body>"
+                                                                  "</html>\r\n\r\n";
+                                            response = malloc(strlen(response_500) + 1);
+                                            strcpy(response, response_500);
+                                        }
+                                            break;
+                                    }
+                                    //PUT response
+
+                                    //404 when the database is empty
+
+                                    //500 if any err has happened that we can not handle
+
+
+
+                                    printf("\nResponse\n%s", response);
+                                    dc_send(&env, &err, client_socket_fd, response, strlen(response), 0);
+                                    free(response);
                                     dc_dc_close(&env, &err, client_socket_fd);
+                                    exit_flag = 0;
                                 } else {
                                     if (err.type == DC_ERROR_ERRNO && err.errno_code == EINTR) {
                                         dc_error_reset(&err);
@@ -133,50 +218,106 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
-void receive_data(struct dc_posix_env *env, struct dc_error *err, int fd, size_t size) {
+int receive_data(struct dc_posix_env *env, struct dc_error *err, int fd, size_t size) {
     // more efficient would be to allocate the buffer in the caller (main) so we don't have to keep
     // mallocing and freeing the same data over and over again.
-
     char *data;
     ssize_t count;
-
+    int return_value;
     data = dc_malloc(env, err, size);
-
+    memset(data, 0, size);
     while (!(exit_flag) && (count = dc_read(env, err, fd, data, size)) > 0 && dc_error_has_no_error(err)) {
         dc_write(env, err, STDOUT_FILENO, data, (size_t) count);
-
-        const char s[1] = "\n";
+        //  &data[length - count]
         char *token;
+        char *tokenBody;
         char *lastToken;
+        char *ptr;
 
-        /* get the first token */
-        token = strtok(data, s);
-        printf("\nfirst tooooooooooooooooooooooooooooooooken: %s\n", token);
+        char buff[50];
 
-        char delimiter[] = " ";
+
+        //----------------test area-------------------
+
+
+
+        //----------------test area-------------------
+
+        //1-check if it's GET or PUT
+        // walk through tokens
+        token = strtok_r(data, "\r\n", &data);
+        printf("\nfirst token: %s\n", token);
+
         char *firstWord;
         int inputLength = strlen(token);
         char *inputCopy = (char *) calloc(inputLength + 1, sizeof(char));
         strncpy(inputCopy, token, inputLength);
-
-        firstWord = strtok(inputCopy, delimiter);
+        firstWord = strtok(inputCopy, " ");
         printf("\nfirst world: %s\n", firstWord);
 
-        /* walk through other tokens */
-        while (token != NULL) {
-            printf("Token: %s\n", token);
+        if (strcmp(firstWord, "GET") == 0) {
+            char *file_name = strtok(inputCopy + strlen(firstWord) + 2, " ");
+            printf("\nBody is: %s\n", file_name);
+            return_value = 1;
+        } else if (strcmp(firstWord, "PUT") == 0) {
+            // 2- getting the size of body
+            char *sub;
+            sub = strstr(data, "Content-Length: ");
+            if (sub != NULL) {
+                // printf("\nSubstring is: %s", sub + 17);
+                char *begin = strchr(sub, ' ');
+                char *end = strchr(begin, '\r');
 
-            lastToken = token;
-            token = strtok(NULL, s);
+//                memcpy(buff, begin, (unsigned long) (end - begin));
+//                printf("\nContent-Length is: %s", buff);
+
+                //3-Tokenize the body----------------------
+                char *body;
+                body = strstr(data, "\r\n\r\n");
+                printf("\nBody is: %s\n", body + 4);
+
+
+                if (body != NULL) {
+                    char *modBody = body + 4;
+                    char *major = strtok(modBody, "=");
+                    char *minor = strtok(NULL, "&");
+                    char *longitude = strtok(NULL, "=");
+                    char *latitude = strtok(NULL, "&");
+                    printf("\nmajor: %s\n", major);
+                    printf("\nminor: %s\n", minor);
+                    printf("\nlongitude: %s\n", longitude);
+                    printf("\nlatitude: %s\n", latitude);
+                }
+
+//                sub = strstr(body + 4, "latitude=");
+////               // tokenBody = strtok_r(body + 4, "-", &body);
+//                char *begin_lat = strchr(sub, '=');
+//                char *end_lat = strchr(begin_lat, '&');
+//                char longitude_number[end_lat - begin_lat-1];
+//                printf("\nlengthhhhhhhhhh: %ld\n", end_lat - begin_lat);
+//                strncpy(longitude_number, begin_lat, (unsigned long) (end_lat - begin_lat));
+//                printf("\nlatitude: %s\n", longitude_number);
+            }
+            return_value = 2;
         }
+
+
+        while (token) {
+            printf("token: %s %lu\n", token, strlen(token));
+            token = strtok_r(data, "\r\n", &data);
+            lastToken = token;
+        }
+
         printf("\nlast token: %s\n", lastToken);
 
-        Read_Write_dbm(env, err, firstWord, lastToken, fd);
-        memset(data, 0, size);
+        //Read_Write_dbm(env, err, firstWord, lastToken, fd);
+        exit_flag = 1;
     }
 
-    dc_free(env, data, size);
+    // dc_free(env, data, size);
+    return return_value;
 }
+
 
 static void quit_handler(int sig_num) {
     exit_flag = 1;
@@ -191,3 +332,56 @@ static void trace_reporter(const struct dc_posix_env *env, const char *file_name
                            const char *function_name, size_t line_number) {
     fprintf(stderr, "Entering: %s : %s @ %zu\n", file_name, function_name, line_number);
 }
+
+
+//connect
+//read
+//write
+//close
+
+
+//
+//    char response[] = "HTTP/1.0 200 OK\r\n"
+//                      "Date: Monday, 24-Apr-95 12:04:12 GMT\r\n"
+//                      "Content-type: text/html\r\n"
+//                      "\r\n"
+//                      "message body"
+//                      "\r\n\r\n";
+//    printf("Greeting message: %s\n", response);
+//
+//    dc_send(env, err, fd, &response, strlen(response), 0);
+/*
+ *
+int strncmp(const char *str1, const char *str2, size_t n)
+
+Parameters
+
+str1 − This is the first string to be compared.
+
+str2 − This is the second string to be compared.
+
+n − The maximum number of characters to be compared.
+
+Return Value
+
+This function return values that are as follows −
+
+if Return value < 0 then it indicates str1 is less than str2.
+
+if Return value > 0 then it indicates str2 is less than str1.
+
+if Return value = 0 then it indicates str1 is equal to str2.
+*/
+
+
+//The C library function strcspn() calculates the length of the number of characters before the 1st occurrence of character present in both the string.
+
+
+/*
+strcmp vs strncmp
+* bothe of them compare from the first char, not string contain
+strcmp compares both the strings till null-character of either string comes, whereas strncmp compares at most num characters of both strings.
+ But if num is equal to the length of either string than strncmp behaves similar to strcmp.
+Problem with strcmp function is that if both of the strings passed in the argument is not terminated by null-character, then comparison of
+ characters continues till the system crashes. But with strncmp function we can limit the comparison with num parameter.
+*/
